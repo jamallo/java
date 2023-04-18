34e6c0ed978ba6363e272170c6afbb28154ea8f7
jamallo <99762846+jamallo@users.noreply.github.com>
2023-04-18 12:32:09 +0200

    Tipos de variables
    Tipos de variables en JAVA

---
+++ Tipos.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        //numéricos
+
+            // enteros
+            byte variable1 = 4;
+        System.out.println("el dato byte es: " + variable1);
+            short variable2 = 10;
+        System.out.println("el dato short es: " + variable2);
+            int variable3 = 30;
+        System.out.println("el dato int es: " + variable3);
+            long variable4 = 100;
+        System.out.println("el dato long es: " + variable4);
+            // decimales
+            float variable5 = 5.5f;
+        System.out.println("el dato float es: " + variable5);
+            double variable6 = 10.5d;
+        System.out.println("el dato double es: " + variable6);
+
+        //booleano
+            boolean variable7 = false;
+        System.out.println("el dato booleano falso es: " + variable7);
+            boolean variable8 = true;
+        System.out.println("el dato booleano verdadero es: " + variable8);
+
+        //texto
+            char variable9 = 'a';
+        System.out.println("el dato char es: " + variable9);
+            String variable10 = "hola que tal?";
+        System.out.println("el dato string es: " + variable10);
+
+    }
+}
