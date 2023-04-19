99601350aab14fa0e63dbe34676987ad410fd5f7
jamallo <99762846+jamallo@users.noreply.github.com>
2023-04-19 08:50:34 +0200

    Add files via upload

---
+++ Condicionales.java
@@ -0,0 +1,12 @@
+package com.example.estructurasControl;
+
+public class Condicionales {
+    public static void main(String[] args) {
+
+        String [] nombres = {"Alba", "María", "Daniel", "Gloria", "Venancio"};
+
+                for (String nombre : nombres) {
+                    System.out.print(nombre + ", ");
+                }
+    }
+}
